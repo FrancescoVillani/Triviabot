f6e66f0464a2d69d65411fdd373ddaf95525e4b4
Davide Checchia <davide.checchia1@gmail.com>
2020-07-10 20:36:41 +0200

    Pseudo-ricerca delle risposte in una ricerca google della domanda

--- main.py
+++ main.py
@@ -1,13 +1,86 @@
-from googleapiclient.discovery import build
+from pip._internal import main as pip
+import json
+from datetime import date
 from pprint import pprint
 
+##Prova ad importare wikipedia, se non c'è allora installala da pip
+try:
+    import wikipedia
+except ImportError:
+    pip(['install', 'wikipedia'])
+
+from googleapiclient.discovery import build
+import wikipedia
+
+##Costanti + setting di wikipedia
+wikipedia.set_lang('it')
 API_key = "AIzaSyBbmlLp4Yxrx_2sE3ebdx9mCt1fL1YrRS4"
 CSE_id = "017297845227077967885:8bnf4eoxf-4"
 
-def search(search_term):
+
+def search(search_term, **kwargs):
     service = build("customsearch", "v1", developerKey=API_key)
-    res = service.cse().list(q=search_term, cx=CSE_id, num=10).execute()
+    res = service.cse().list(q=search_term, cx=CSE_id, **kwargs).execute()
     return res['items']
 
-results = search('how old is grandpa pig')
-pprint(results[0])
+
+def Question():
+    return "Qual è la capitale dell'Italia?"
+
+def Answers():
+    return ["Roma", "Parigi", "Berlino"]
+
+def Magic(q): return q.replace("Qual è la ","").replace("dell'","")
+
+
+##Prendi la domanda, somehow
+question = Question()
+
+##Togli le robe inutili, lascia i sostantivi e kwords
+question_strip = Magic(question)
+
+##Prendi le risposte
+answers = Answers()
+
+##Prendi i primi num risultati di Google
+results = search(question_strip, num=3)
+
+##Crea set risposte
+correct_ans = set()
+
+
+##Considera ogni risposta disponibile, prendi i risultati di ricerca
+##Se trovi le risposte nei risultati di ricerca prob è quella giusta
+##I risultati sono dict (JSON) con title, body etc quindi serve element in res.values()
+##Aggiungo le risposte in un set
+for ans in answers:
+    for res in results:
+        for tag in res.values():
+            if ans in tag:
+                correct_ans.add(ans)
+
+
+##Se il set è di dimensione 1, vuol dire che ha trovato solo una risposta
+if len(correct_ans)==1
+    right = list(correct_ans)[0]
+else:
+    right = "BAD"
+
+print("{0}\n\n{1}\n\nLa risposta è: {2}".format(question, answers, right))
+
+
+
+# results = search("Età obama")
+#
+#
+# for result in results:
+#     if "wikipedia" in result['title'].lower():
+#         print(result['title'])
+#         valid.append(wikipedia.search(result['title'].lower().replace("- wikipedia", ""), results=1))
+#         break
+#
+# for title in valid:
+#     print(title)
+#     print(wikipedia.page(title).content[:300])
+
+
